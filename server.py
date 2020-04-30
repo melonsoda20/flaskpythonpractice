56d151f08a1cc3426325f3d60205364d6ba2fccd
melonsoda20 <benedictusteddy200596@gmail.com>
2020-04-30 14:46:54 +0700

    Add basic routing and HTML template

--- server.py
+++ server.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 print(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Test123!'
+
+@app.route('/<username>/<int:post_id>')
+def hello_world(username=None, post_id=None):
+    return render_template('./index.html', name=username, post_id = post_id)
+
+
+@app.route('/blog')
+def blog():
+    return 'These are my blog'
+
+
+@app.route('/blog/2020/dogs')
+def blog2():
+    return 'These are my dog blog'
+
+
+@app.route('/about')
+def about():
+    return render_template('./about.html')
