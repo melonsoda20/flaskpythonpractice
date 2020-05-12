80835256a70228ea1524b4cd217dcbab6055ce8d
melonsoda20 <benedictusteddy200596@gmail.com>
2020-05-12 22:20:37 +0700

    Add new route for submitting email

--- server.py
+++ server.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 
 app = Flask(__name__)
 
+extension = '.html'
+
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index' + extension)
 
 
 @app.route('/<string:page_name>')
 def html_page(page_name):
-    return render_template(page_name + '.html')
+    return render_template(page_name + extension)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return 'Form submitted!'
+
+    else:
+        return 'Something went wrong'
 
