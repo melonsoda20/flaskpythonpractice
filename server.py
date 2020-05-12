5570f6682645f0623ec0463de7c8b61d444d8a44
melonsoda20 <benedictusteddy200596@gmail.com>
2020-05-12 21:54:25 +0700

    Implement dynamic routing

--- server.py
+++ server.py
@@ -9,31 +9,7 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/index')
-def index():
-    return render_template('index.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name + '.html')
 
-
-@app.route('/work')
-def work():
-    return render_template('work.html')
-
-
-@app.route('/works')
-def works():
-    return render_template('works.html')
-
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-
-@app.route('/components')
-def components():
-    return render_template('components.html')
